b09b558099e2075b56227cc61714d34ec65391df
Yuliia Hrabovenko <yuliia.hrabovenko@gmail.com>
2025-01-15 16:56:25 +0200

    add logs

--- src/main/java/com/task/springdemo/TestController.java
+++ src/main/java/com/task/springdemo/TestController.java
@@ -1,13 +1,20 @@
 package com.task.springdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping("/message")
     public String getMessage(){
+        log.info("Processing user request to /message endpoint");
         return "Hello, World!";
     }
 }
